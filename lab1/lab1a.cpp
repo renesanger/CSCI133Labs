12e26f02e4ad21ad70a776ca199141c91ef3a4f9
Rene Sanger <renejsanger@gmail.com>
2017-02-02 23:25:15 -0500

    h

--- lab1/lab1a.cpp
+++ lab1/lab1a.cpp
@@ -5,9 +5,10 @@ using namespace std;
   Author: Rene Sanger
   Course: {135,136}
   Instructor: Mike Zamansky
-  Assignment: "Lab 1"
+  Assignment: "Lab 1" part a
 
   This program does ...
+	just says hello to instructor
 */
 
 int main() {
