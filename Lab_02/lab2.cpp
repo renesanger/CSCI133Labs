b1000e127ab166f0cb823b26346f4d77324606ce
Rene Sanger <renejsanger@gmail.com>
2017-02-15 14:01:40 -0500

    h

--- Lab_02/lab2.cpp
+++ Lab_02/lab2.cpp
@@ -30,16 +30,35 @@ int M=100;
 int k=20;
 
 /*
+do{
 cout<<"Enter a Population size:"<<endl;
 cin>>P;
+}
+while(P<0);
+
+do{
 cout<<"Growth rate:"<<endl;
 cin>>G;
+}
+while(G<0);
+
+do{
 cout<<"Enter a minimum losss rate:"<<endl;
 cin>>H;
+}
+while(H>1 || H<0);
+
+do{
 cout<<"Enter a carrying capacity:"<<endl;
 cin>>M;
+}
+while(M<0);
+
+do{
 cout<<"Enter the amount of years on the table:"<<endl;
 cin>>k;
+}
+while(k<0);
 */
 
 cout<<"Year"<<"	"<<"Population"<<endl;
